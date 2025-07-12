f11ac61cbcf7d2fcbf32330cef6385dac05e929f
HuangHeidi <129025463+HuangHeidi@users.noreply.github.com>
2025-07-13 03:50:11 +0800

    Update vite.config.js
    Ensure vite.config.js is CommonJS

--- video-management-system/vite.config.js
+++ video-management-system/vite.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react');
+const { defineConfig } = require("vite");
+const react = require("@vitejs/plugin-react");
 
 // https://vitejs.dev/config/
 module.exports = defineConfig({
